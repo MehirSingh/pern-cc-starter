e10fb10752755ec97fc5f7a6ee12ca79b4c1e186
Mehir Singh <mehirmanu2004@gmail.com>
2026-07-20 10:55:38 +0000

    Add React frontend and Vite proxy for PERN app

--- 04_reactjs/src/App.jsx
+++ 04_reactjs/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import Car from './components/Cars.jsx'
+import { useState, useEffect } from "react"
 
 const App = () =>{
+  const[Cars, setCars] = useState([])
+
+  useEffect(()=>{
+    fetch('api/v1/Cars')
+      .then(res => res.json())
+      .then(data => setCars(data))
+      .catch(err => console.log(err))
+  },[]);
+
+   console.log(Cars);
+
   return(
     <div>
       <h1>Welcome to the Car Stor </h1>
-      <ul>Cars</ul>
-      <ul>Cars</ul>
-      <ul>Cars</ul>
-      <ul>Cars</ul>
+      <ul>
+        {Cars.map(car =>(
+            <Car key={car.id} {...car} />
+        ))}
+      </ul>
       </div>
   )
 }
--- 04_reactjs/src/components/cars.jsx
+++ 04_reactjs/src/components/cars.jsx
@@ -1,7 +1,11 @@
-const Car = () =>{
+const Car = ( {make, model, year, price} ) =>{
     return(
-        <div>Car</div>
+        <li>
+        <p>Make: {make}</p>
+        <p>Model: {model}</p>
+        <p>Year: {year}</p>
+        <p>Price: {price}</p>
+        </li>
     )
 }
-
 export default Car
